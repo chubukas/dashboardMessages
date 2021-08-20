1c133a625723161e625a444a59d6639e320d7468
chukwuebuka Anyadiegwu <chubukass@gmail.com>
2021-08-21 00:44:38 +0100

    feat(data): add data
    developer the data to be used in the project

---
+++ src/data/data.js
@@ -0,0 +1,69 @@
+export const data = {
+  notifications: [
+    {
+      name: "Marvic Mckinney",
+      message: "a good man is good",
+      time: "05:51",
+      socialMedia: "Facebook",
+      status: "Open",
+      unreadMessage: 1,
+      pics: "/images/1.jpg",
+    },
+    {
+      name: "Marvic Mckinney",
+      message: "a good man is good",
+      time: "05:51",
+      socialMedia: "Facebook",
+      status: "In Progress",
+      unreadMessage: 1,
+      pics: "/images/1.jpg",
+    },
+    {
+      name: "Jerome Bell",
+      message: "a good man is good",
+      time: "2 days ago",
+      socialMedia: "Email",
+      status: "Close",
+      unreadMessage: 0,
+      pics: "/images/1.jpg",
+    },
+    {
+      name: "Marvic Mckinney",
+      message: "a good man is good",
+      time: "a week ago",
+      socialMedia: "Facebook",
+      status: "Close",
+      unreadMessage: 0,
+      pics: "/images/1.jpg",
+    },
+  ],
+
+  messages: {
+    name: "Jerome Bell",
+    socialMedia: "email",
+    time: "sat 13 mar 2021 at 10:54 Am",
+    subject: "How can I get a refund for my order?",
+    TicketId: "#53467 ",
+    pics: "/images/1.jpg",
+    mainMessage: `
+    Hi there,
+      
+    I need a refund for the head phones that i purshased last week my order Id is #53467.
+    The product was damage when i received it. Can yo please tell me how i can get a refund?
+    
+    Best,
+    Jerome`,
+
+    replies: [
+      {
+        name: "Hammed Dauda",
+        socialMedia: "Email",
+        time: "16 days ago sat 13 mar 2021 at 12:54 pm",
+        replayMessage: `ghddfgdfgdhsdghabsjhdbhb chc bhscbshdfgyewgdyeduefsnhsd csdncfm
+          shcbsdhcfsdbfhsdbfjhdfdhfbdhfbdhfdfgyfdgdyegdeywfehfhfdbfhdbfjhdfdf
+          dysdgyfsgdfydgfhdfsbdfhdbfhdbjddbfhdjf`,
+        pics: "",
+      },
+    ],
+  },
+};
